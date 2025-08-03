98ac1525df1692ff5e6b9f4a05bd7b4e41b0826d
Saeed Kabootari <saeedkabootari@gmail.com>
2025-08-03 14:37:39 +0330

    change logout button

--- src/layouts/LayoutMenu.jsx
+++ src/layouts/LayoutMenu.jsx
@@ -31,13 +31,13 @@ const LayoutMenu = (props) => {
           <span className="text-3xl text-white font-semibold">
             {t("Aiat system")}
           </span>
-          <button
+          {/* <button
             onClick={logoutHandler}
             className="bg-red-500 rounded p-1 text-white"
           >
             logout
-          </button>
-          {/* <button onClick={logoutHandler} className="rounded p-1 text-red-500">
+          </button> */}
+          <button onClick={logoutHandler} className=" text-red-500 cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -52,7 +52,7 @@ const LayoutMenu = (props) => {
                 d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
               />
             </svg>
-          </button> */}
+          </button>
         </header>
         {/* Main Content Area with Sidebar */}
         <div
